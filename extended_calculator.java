4a728ecd4a328fd7b2357f2086a58887542ba658
01FE22BCA170 <156194110+01FE22BCA170@users.noreply.github.com>
2024-03-18 10:44:35 +0530

    Add files via upload

---
+++ extended_calculator.java
@@ -0,0 +1,13 @@
+
+
+public class extended_calculator{
+    float square_root(float opr1)
+      {
+     return  (opr1*opr1) ;
+      }
+ 
+   float cube_root(float opr1)
+       {
+     return (opr1*opr1*opr1);
+       }
+    }
